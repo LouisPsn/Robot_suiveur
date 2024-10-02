87dd5069b9d2e4890dbefa224d1610555d9d511e
LouisPsn <lpierson002@enseirb-matmeca.fr>
2024-10-02 14:46:19 +0200

    Debug motor

--- motor.py
+++ motor.py
@@ -10,5 +10,5 @@ dxl_io = pypot.dynamixel.DxlIO(ports[0])
 
 dxl_io.set_wheel_mode([1])
 dxl_io.set_moving_speed({1: 360}) # Degrees / s
-dxl_io.set_moving_speed({2: 360}) # Degrees / s
+dxl_io.set_moving_speed({2: -360}) # Degrees / s
 
