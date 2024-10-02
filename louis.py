d6cfc450ccb25b1befe75045be1638fe600afcf3
LouisPsn <lpierson002@enseirb-matmeca.fr>
2024-10-02 14:53:59 +0200

    Test 1 suivi ligne

--- louis.py
+++ louis.py
@@ -7,11 +7,10 @@ import pypot.dynamixel
 cap = cv2.VideoCapture(0)
 
 # Set up motors
-# ports = pypot.dynamixel.get_available_ports()
-# if not ports:
-#     exit('No port')
-# dxl_io_L = pypot.dynamixel.DxlIO(ports[0])
-# dxl_io_R = pypot.dynamixel.DxlIO(ports[1])
+ports = pypot.dynamixel.get_available_ports()
+if not ports:
+    exit('No port')
+dxl_io = pypot.dynamixel.DxlIO(ports[0])
 
 
 def analyse_image():
@@ -122,10 +121,9 @@ def compute_speed(er):
     return vL, vR
 
 def command_motors(vL, vR):
-    dxl_io_L.set_wheel_mode([1])
-    dxl_io_R.set_wheel_mode([1])
-    dxl_io_L.set_moving_speed({1: vL}) # Degrees / s
-    dxl_io_R.set_moving_speed({1: vR}) # Degrees / s
+    dxl_io.set_wheel_mode([1])
+    dxl_io.set_moving_speed({1: -vL}) # Degrees / s
+    dxl_io.set_moving_speed({2: vR}) # Degrees / s
 
 
 def main():
