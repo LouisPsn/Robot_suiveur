f5e11f92980ce99977e73f8d329bf2a81c40ee60
Egouhey <estebane.gouhey@gmail.com>
2024-10-02 17:11:01 +0200

    test_encodeuse

--- estebane.py
+++ estebane.py
@@ -7,9 +7,18 @@ if not ports:
     exit('No port')
 dxl_io = pypot.dynamixel.DxlIO(ports[0])
 
+found_ids = dxl_io.scan()
+ids = found_ids[:2]
+
 
 
 # qzlrilibz
 dxl_io.set_wheel_mode([1])
 dxl_io.set_moving_speed({1: -360}) # Degrees / s
 dxl_io.set_moving_speed({2: 360}) # Degrees / s
+
+while True :
+    print(dxl_io.get_present_position(1))
+    print(dxl_io_get_present_state(2))
+    time.sleep(1)
+
