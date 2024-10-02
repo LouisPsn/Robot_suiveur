95708307772dc68c0f84158417d226ef6d8b7dbc
Egouhey <estebane.gouhey@gmail.com>
2024-10-02 17:54:34 +0200

    test

--- estebane.py
+++ estebane.py
@@ -12,7 +12,7 @@ dxl_io = pypot.dynamixel.DxlIO(ports[0])
 
 
 # qzlrilibz
-dxl_io.set_wheel_mode([3])
+dxl_io.set_wheel_mode([2])
 #dxl_io.set_moving_speed({1: -360}) # Degrees / s
 #dxl_io.set_moving_speed({2: 360}) # Degrees / s
 
