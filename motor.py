f0e8a8b26ee9cd9adb797bf36ad07ddab344e60c
LouisPsn <lpierson002@enseirb-matmeca.fr>
2024-10-02 14:45:54 +0200

    Debug motor

--- motor.py
+++ motor.py
@@ -6,7 +6,6 @@ ports = pypot.dynamixel.get_available_ports()
 if not ports:
     exit('No port')
 dxl_io = pypot.dynamixel.DxlIO(ports[0])
-dxl_io = pypot.dynamixel.DxlIO(ports[1])
 
 
 dxl_io.set_wheel_mode([1])
