0dc2fe0835d860b5a016fdd3bfd3d531d37fb36d
Egouhey <estebane.gouhey@gmail.com>
2024-10-03 16:46:51 +0200

    odom et goto

--- perso/estebane.py
+++ perso/estebane.py
@@ -1,64 +1,85 @@
 import pypot.dynamixel
+import matplotlib.pyplot as plt
+import math
 import time
-import numpy as np
+
+# définition des constantes
+wheelDiameter = 5.2 # in cm
+wheelDiameterSI = (wheelDiameter / 100)/2 # in m
+wheelDistance = 14.5 # in cm
+wheelDistanceSI = (wheelDistance/100)
+frequency = 2.5 # in Hz
+
+motorId = [1,2]
+
+# Initilisation des moteurs
+def init(motors:list):
+    ports = pypot.dynamixel.get_available_ports()
+    if not ports:
+        exit('No port')
+    print(ports)
+
+    dxl_io = pypot.dynamixel.DxlIO(ports[0])
+    dxl_io.disable_torque(motors)
+    dxl_io.set_wheel_mode([1])
+    return dxl_io
+
+
+dxl = init(motorId)
+worldX = 0
+worldY = 0
+worldTeta = 0
 
 
 
-# Set up motors
-ports = pypot.dynamixel.get_available_ports()
-if not ports:
-    exit('No port')
-dxl_io = pypot.dynamixel.DxlIO(ports[0])
+Position=[]
 
-current_position_1=(0,0)
-current_position_2=(0,0)
 
-past_position_1=0
-past_position_2=0
+# récupère la vitesse des moteurs et redonne les vitesses linéaires et angulaires robot
+def wheelSpeedConvertion(leftWheel, RightWheel):
+    leftWheelRad = leftWheel * (math.pi/180)        
+    rightWheelRad = RightWheel * (math.pi/180)
+    linearSpeed = (wheelDiameter * (leftWheelRad+rightWheelRad))/4
+    angularSpeed = ((wheelDiameter * (leftWheelRad-rightWheelRad))/wheelDistance)/2
 
-# distance en centimetre
-empatement = 14.5
-perimetre_empatement = (empatement/2)*2*3.1415
+    return linearSpeed, angularSpeed
 
-diametre_roue = 5.2
-rayon_roue = diametre_roue/2
-perimetre_roue = rayon_roue*2*3.1415
+# calcul x, y et theta actuel
+def speedToDelta(linearSpeed, angularSpeed, dt):
+    '''
+        if angularSpeed == 0: # TODO ajouter une tolerance
+        x = linearSpeed*dt*math.cos(worldTeta)
+        y = linearSpeed*dt*math.sin(worldTeta)
+        return (x,y,0)
+    else:
 
-angle_robot=0
+    '''
+    teta = angularSpeed*dt
+    x = (linearSpeed*dt)*(math.cos(worldTeta + angularSpeed*dt))
+    y = (linearSpeed*dt)*(math.sin(worldTeta + angularSpeed*dt))
+    return(x,y,teta)
 
-angletotal_roue_1 = 0
-angletotal_roue_2 = 0
+# Récupération des données et stockage dans Position
+for i in range(0,100):
+    leftSpeed, rightSpeed = dxl.get_present_speed([1,2])
+    leftSpeed = -leftSpeed
+    v,teta = wheelSpeedConvertion(rightSpeed, leftSpeed)
+    dx,dy,dteta = speedToDelta(v,teta,1/frequency)
+    worldX += dx
+    worldY += dy
+    worldTeta += dteta
+    Position.append( (worldX, worldY) )
+    print("{}, {}, {}".format(worldX,worldY,worldTeta/(math.pi/180)))
+    time.sleep(1/frequency)
 
-# qzlrilibz
-dxl_io.set_wheel_mode([2])
-#dxl_io.set_moving_speed({1: -360}) # Degrees / s
-#dxl_io.set_moving_speed({2: 360}) # Degrees / s
+# Création et sauvegarde du parcours
+x, y = zip(*Position)
 
-while True :
-    past_position_1 = current_position_1
-    past_position_2 = current_position_2
+plt.plot(x, y, marker='o', linestyle='-', color='b')
 
-    # en degree
-    current_position_1=dxl_io.get_present_position((1, ))
-    current_position_2 =dxl_io.get_present_position((2, ))
+plt.title('Parcours du robot')
+plt.xlabel('Axe X')
+plt.ylabel('Axe Y')
+plt.grid()
 
-    print(current_position_1)
-    print(current_position_2)
-
-    #posera probleme par la suite
-    delta_angle_1 = current_position_1[0] - past_position_1[0]
-    delta_angle_2 = current_position_2[0] - past_position_2[0]
-
-    delta_position_1 = perimetre_roue*(delta_angle_1/360)
-    delta_position_2 = perimetre_roue*(delta_angle_2/360)
-
-    angle = angle + np.degrees(np.arcsin( ( delta_position_1 - delta_position_2)/rayon_roue ))
-    #PositionX = (delta_1 + delta_2)*np.cos(angle)
-    #PositionY = (delta_1 + delta_2)*np.sin(angle)
-
-    print(angle)
-    #print(PositionX)
-    #print(PositionY)
-
-    time.sleep(1)
-
+plt.savefig('parcours.png')
