7104ce0e8ccd10ec7eaba61251e511c9b85530c4
LouisPsn <lpierson002@enseirb-matmeca.fr>
2024-10-03 17:08:33 +0200

    Debug

--- perso/louis/goto_louis.py
+++ perso/louis/goto_louis.py
@@ -44,7 +44,13 @@ def compute_motor_command_1(x, y, theta_util):
     
     
     # Calcule des coordonnées du cercle suivi par le robot
-    theta_i = math.atan(x/y)
+    if y != 0:
+        theta_i = math.atan(x/y)
+    else:
+        if x > 0:
+            theta_i = math.pi/2
+        else:
+            -theta_i = math.pi/2
     distance = math.sqrt(x**2 + y**2)
     theta = math.pi - 2*(math.pi/2 - theta_i)
     r = (distance/2)/math.sin(theta/2)
