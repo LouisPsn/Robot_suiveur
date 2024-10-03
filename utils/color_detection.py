4774f259bf5a905123f6ecdb65ec6192309e25e5
Guillaume Dindart <guillaume.dindart@etu.u-bordeaux.fr>
2024-10-03 14:12:00 +0200

    try black

--- utils/color_detection.py
+++ utils/color_detection.py
@@ -9,7 +9,9 @@ def getHisto(color_h_min:int, color_h_max:int, frame):
     s = cv2.extractChannel(hsv,1)
 
     cv2.imshow("h", h)
-    cv2.imshow("s", s)
+    #cv2.imshow("s", s)
+    #cv2.imshow("v", v)
+
     # Threshold of blue in HSV space 
     # lower_blue = np.array([60, 140, 160]) 
     # upper_blue = np.array([180, 255, 255])  
@@ -17,25 +19,30 @@ def getHisto(color_h_min:int, color_h_max:int, frame):
   
     # preparing the mask to overlay 
     mask1 = cv2.inRange(h, np.array([color_h_min]), np.array([color_h_max]))
-    cv2.imshow("mask1", mask1)
     
-    mask2 = cv2.inRange(s, np.array([20]), np.array([255]))
-    cv2.imshow("mask2", mask2)
+    
+    mask2 = cv2.inRange(s, np.array([0]), np.array([255]))
+    
     
     mask = cv2.bitwise_and(mask1, mask2)
     mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    cv2.imshow("mask", mask)
     frame = cv2.bitwise_and(frame, mask)
     return frame
     
     
 
-img = cv2.imread("./parcours.png")
-cv2.imshow("base",img)
+# img = cv2.imread("./parcours.png")
+# cv2.imshow("base",img)
 #cv2.imshow("red",getHisto(0,20, img))
-cv2.imshow("green",getHisto(80,120, img))
+# cv2.imshow("green",getHisto(80,120, img))
 #cv2.imshow("blue",getHisto(160,180, img))
 
+cap = cv2.VideoCapture(5)
+
 while(True):
-    if(cv2.waitKey()):
-        break
+    _, frame = cap.read()
+    cv2.imshow("base",frame)
+    cv2.imshow("result", getHisto(140, 180, frame))
+    cv2.waitKey(14)
 
--- utils/display_camera.py
+++ utils/display_camera.py
@@ -37,7 +37,7 @@ while(1):
     _, frame = cap.read() 
     cv2.imshow('frame', frame) 
 
-    cv2.waitKey(14)
+    cv2.waitKey()
 
 cv2.destroyAllWindows() 
 cap.release()
